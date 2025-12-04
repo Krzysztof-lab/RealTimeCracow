0a2cb0f93ae9b159d5534eedccdf8b651e7da366
Krzysztof-lab <kpieczka@student.agh.edu.pl>
2025-12-04 21:29:20 +0100

    spring init

---
+++ RealTimeCracow/src/test/java/pl/edu/agh/to/realtimecracow/RealTimeCracowApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.to.realtimecracow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeCracowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
